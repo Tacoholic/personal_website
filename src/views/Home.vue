c44126308602e216d6a81f5c392dd90bd7d7cd56
Tacoholic <solischristian@gmail.com>
2019-05-06 23:25:12 -0500

    added fonts and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,91 @@
 <template>
   <body>
-
-    <title>Christian Solis</title>
+  <title>Christian Solis</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bitter|Montserrat" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
 
   <!-- Sidebar -->
-  <div class="w3-sidebar w3-yellow w3-bar-block" style="width:30%">
-    <div class="w3-bar-item"><h1>CHICAGOAN.</h1></div>
-    <div class="w3-bar-item"><h1>DEVELOPER</h1></div>
-    <div class="w3-bar-item"><h1>& TACOHOLIC</h1></div>
-    <p>I am a graduate from <a href="https://www.https://anyonecanlearntocode.com/" class="link">Actualize.</a></p>
-    
+ 
+  <div>
+    <div class="about">
+      <h1>CHICAGOAN.</h1>
+      <h1>FRONT END DEVELOPER.</h1>
+      <h1>SPORTS FANATIC</h1>
+      <h1>& TACOHOLIC.</h1>
+    </div>
+  </div>
+
+  <div class="solis">
     <p>Former producer at <a href="https://www.espn.com" class="link">ESPN.</a></p>
+    <p> <a href="https://www.https://anyonecanlearntocode.com/" class="link">Actualize </a>Coding Bootcamp graduate.</p>
     <p>Follow me on my new journey in the tech world.</p> 
   </div>
-
-  <!-- Page Content -->
-  <div style="margin-left:30%">
-
   <div>
-  
+    <a href="https://github.com/Tacoholic"> <i class="fa fa-github" style="font-size:50px;color:red"></i></a>
   </div>
 
-  <!-- <img src="img_car.jpg" alt="Car" style="width:100%"> -->
+  <div>
+    <a href="https://medium.com/@christiansolis_53189"> <i class="fa fa-medium" style="font-size:50px;color:red"></i></a>
+  </div>
 
-  <div class="w3-container">
+
+ <div>
+  <a class="btn btn-primary" href="mailto:@solischristian@gmail.com" role="button">Say hello!</a>
+  </div>
+</div>
+
+  
+  <!-- Page Content -->
+
+  <!-- <div class="w3-container" style="margin-left:50%">
   <h2>Sidebar Navigation Example</h2>
   <p>The sidebar with is set with "style="width:25%".</p>
   <p>The left margin of the page content is set to the same value.</p>
-  </div>
-
-  </div>
-        
+  </div>  -->       
   </body>
+
+
+
 </template>
 
 
 
-<script>    
+<script>   
+
+
 </script>
+<style>
+.about{
+  margin-top: 120px;
+  margin-left: 110px;
+  font-family: 'Bitter', serif;
+}
+
+.solis{
+  margin-top: 25px;
+  margin-left: 115px;
+  font-family: 'Montserrat', sans-serif;
+}
+
+.btn {
+ margin-top: 25px;
+ margin-left: 200px;
+ }
+.fa-medium {
+  margin-top: -150px;
+  margin-left: 350px;
+
+ }
+
+ .fa-github {
+  margin-top: 20px;
+  margin-left: 200px;
+
+ }
+
+
+
+</style>
