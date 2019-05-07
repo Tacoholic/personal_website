fadab791afb5f5462242c4ebfd6e72d21cc9629e
Tacoholic <solischristian@gmail.com>
2019-05-07 14:45:25 -0500

    adjusted icons and button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,8 @@
  
 
   <!-- Sidebar -->
+
+
  
   <div>
     <div class="about">
@@ -23,40 +25,28 @@
     <p> <a href="https://www.https://anyonecanlearntocode.com/" class="link">Actualize </a>Coding Bootcamp graduate.</p>
     <p>Follow me on my new journey in the tech world.</p> 
   </div>
-  <div>
-    <a href="https://github.com/Tacoholic"> <i class="fa fa-github" style="font-size:50px;color:red"></i></a>
+
+
+  <div class="icons">
+    <span><a href="https://github.com/Tacoholic"><i class="fa fa-github" style="font-size:45px;"></i></a></span>
+    <span><a href="https://medium.com/@christiansolis_53189"><i class="fa fa-medium" style="font-size:45px;"></i></a></span>
   </div>
 
   <div>
-    <a href="https://medium.com/@christiansolis_53189"> <i class="fa fa-medium" style="font-size:50px;color:red"></i></a>
+    <a class="btn btn-primary" href="mailto:@solischristian@gmail.com" role="button">Say hello!</a>
   </div>
 
-
- <div>
-  <a class="btn btn-primary" href="mailto:@solischristian@gmail.com" role="button">Say hello!</a>
-  </div>
-</div>
-
-  
-  <!-- Page Content -->
-
-  <!-- <div class="w3-container" style="margin-left:50%">
-  <h2>Sidebar Navigation Example</h2>
-  <p>The sidebar with is set with "style="width:25%".</p>
-  <p>The left margin of the page content is set to the same value.</p>
-  </div>  -->       
+      
   </body>
 
-
-
 </template>
-
 
 
 <script>   
 
 
 </script>
+
 <style>
 .about{
   margin-top: 120px;
@@ -69,23 +59,23 @@
   margin-left: 115px;
   font-family: 'Montserrat', sans-serif;
 }
-
-.btn {
- margin-top: 25px;
- margin-left: 200px;
- }
-.fa-medium {
-  margin-top: -150px;
-  margin-left: 350px;
-
- }
-
+ 
  .fa-github {
-  margin-top: 20px;
-  margin-left: 200px;
-
+  margin-top: 30px;
+  color:red;
+  margin-left: 180px;
  }
 
+ .fa-medium {
+margin-top: 30px;
+  color:red;
+  margin-left: 160px;
+ }
+
+ .btn {
+  margin-top: 50px;
+  margin-left: 255px;
+  }
 
 
 </style>
