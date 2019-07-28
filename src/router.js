dcfa1de7d4aba6ab4b81a3a6d981e8111672955c
Tacoholic <solischristian@gmail.com>
2019-07-28 16:47:58 -0500

    Current Projects Page

--- src/router.js
+++ src/router.js
@@ -17,10 +17,17 @@ export default new Router({
     {
       path: '/CurrentProjects',
       name: 'CurrentProjects',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/CurrentProjects.vue')
+      component: CurrentProjects
+    
     }
   ]
 })
+
+
+ // path: '/CurrentProjects',
+ //      name: 'CurrentProjects',
+ //      // route level code-splitting
+ //      // this generates a separate chunk (about.[hash].js) for this route
+ //      // which is lazy-loaded when the route is visited.
+ //      component: () => import(/* webpackChunkName: "about" */ './views/CurrentProjects.vue')
+ //    }
